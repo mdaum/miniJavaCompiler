f54e2304a43e478bd604894a4d58d4355558eb95
unknown <daumisgod@gmail.com>
2016-03-15 23:54:01 -0400

    started data structure for scoped id table

---
+++ src/miniJava/ContexualAnalyzer/IDTable.java
@@ -0,0 +1,15 @@
+package miniJava.ContexualAnalyzer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import miniJava.AbstractSyntaxTrees.Declaration;
+
+public class IDTable {
+	
+	ArrayList<HashMap<String,Declaration>> table = new ArrayList<HashMap<String,Declaration>>(); //our main thing
+	
+	public IDTable(){ //here we need to add our predefined classes
+		
+	}
+}
