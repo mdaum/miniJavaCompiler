db1ac5c077a926299ef6dc907fa22445a26d1fed
unknown <daumisgod@gmail.com>
2016-03-23 17:27:28 -0400

    finished a couple more

--- src/miniJava/ContextualAnalyzer/TypeChecker.java
+++ src/miniJava/ContextualAnalyzer/TypeChecker.java
@@ -63,13 +63,13 @@ public class TypeChecker implements Visitor<Object,Type> {
 	@Override
 	public Type visitClassType(ClassType type, Object arg) {
 		// TODO Auto-generated method stub
-		return type;
+		return type.className.d.type;
 	}
 
 	@Override
 	public Type visitArrayType(ArrayType type, Object arg) {
 		// TODO Auto-generated method stub
-		return type.eltType.visit(this, null);
+		return new ArrayType(type.eltType.visit(this, null),type.posn);
 	}
 
 	@Override
@@ -103,20 +103,32 @@ public class TypeChecker implements Visitor<Object,Type> {
 	}
 
 	@Override
-	public Type visitReturnStmt(ReturnStmt stmt, Object arg) {
+	public Type visitReturnStmt(ReturnStmt stmt, Object arg) {//only stmt that returns a type if return expr exists!
 		// TODO Auto-generated method stub
-		return null;
+		if(stmt.returnExpr!=null)return stmt.returnExpr.visit(this, null);
+		return null; //meaning return w no expr/type..
 	}
 
 	@Override
 	public Type visitIfStmt(IfStmt stmt, Object arg) {
 		// TODO Auto-generated method stub
+		Type cond = stmt.cond.visit(this, null);
+		if(cond.typeKind!=TypeKind.BOOLEAN){
+			reporter.reportError("*** Type Check Error: Conditional in if statement not of type boolean! Position: "+stmt.cond.posn);
+		}
+		stmt.thenStmt.visit(this, null);
+		if(stmt.elseStmt!=null)stmt.elseStmt.visit(this, null);
 		return null;
 	}
 
 	@Override
 	public Type visitWhileStmt(WhileStmt stmt, Object arg) {
 		// TODO Auto-generated method stub
+		Type cond = stmt.cond.visit(this, null);
+		if(cond.typeKind!=TypeKind.BOOLEAN){
+			reporter.reportError("*** Type Check Error: Conditional in while loop not of type boolean! Position: "+stmt.cond.posn);
+		}
+		stmt.body.visit(this, null);//seeing if we find error in body
 		return null;
 	}
 
@@ -135,7 +147,7 @@ public class TypeChecker implements Visitor<Object,Type> {
 	@Override
 	public Type visitRefExpr(RefExpr expr, Object arg) {
 		// TODO Auto-generated method stub
-		return null;
+		return expr.ref.visit(this, arg);
 	}
 
 	@Override
@@ -147,23 +159,29 @@ public class TypeChecker implements Visitor<Object,Type> {
 	@Override
 	public Type visitLiteralExpr(LiteralExpr expr, Object arg) {
 		// TODO Auto-generated method stub
-		return null;
+		return expr.lit.visit(this, arg);
 	}
 
 	@Override
 	public Type visitNewObjectExpr(NewObjectExpr expr, Object arg) {
 		// TODO Auto-generated method stub
-		return null;
+		return expr.classtype.visit(this, arg);
 	}
 
 	@Override
-	public Type visitNewArrayExpr(NewArrayExpr expr, Object arg) {
+	public Type visitNewArrayExpr(NewArrayExpr expr, Object arg) {//should hand back arrayType
 		// TODO Auto-generated method stub
-		return null;
+		Type size = expr.sizeExpr.visit(this, arg);
+		Type inner = expr.eltType.visit(this, arg);
+		if(size.typeKind!=TypeKind.INT){
+			reporter.reportError("*** Type Checking Error: Size of array must be an int! Position: "+expr.sizeExpr.posn);
+			return genError;
+		}
+		return new ArrayType(inner,expr.posn);
 	}
 
 	@Override
-	public Type visitQualifiedRef(QualifiedRef ref, Object arg) {
+	public Type visitQualifiedRef(QualifiedRef ref, Object arg) {//coming back
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -174,10 +192,7 @@ public class TypeChecker implements Visitor<Object,Type> {
 		Type toReturn=null;
 		Type e=ref.indexExpr.visit(this,null);
 		Type r = ref.idRef.visit(this, arg);
-		if(e.typeKind==TypeKind.ERROR||r.typeKind==TypeKind.ERROR||r.typeKind==TypeKind.UNSUPPORTED||e.typeKind==TypeKind.UNSUPPORTED){
-			toReturn=genError;
-		}
-		else if(e.typeKind!=TypeKind.INT){
+		if(e.typeKind!=TypeKind.INT){
 			toReturn=genError;
 			reporter.reportError("*** Type Checking Error: Cannot have a "+e.typeKind+" as an index! Position: "+ref.indexExpr.posn);
 		}
@@ -189,7 +204,7 @@ public class TypeChecker implements Visitor<Object,Type> {
 	public Type visitIdRef(IdRef ref, Object arg) {
 		// TODO Auto-generated method stub
 		Type id = ref.id.visit(this, null);
-		if(id.typeKind==TypeKind.UNSUPPORTED||id.typeKind==TypeKind.ERROR){
+		if(id.typeKind==TypeKind.ERROR){
 			return genError; //will check error all the way up...
 		}
 		return id;
